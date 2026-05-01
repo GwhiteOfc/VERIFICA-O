32d40a2e36841373cb91046ef0cbebcf0083346c
GwhiteOfc <ncage5452@gmail.com>
2026-05-01 00:26:36 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    token: "SEU-TOKEN-AQUI",
+    id: "SEU-ID-DO-BOT",
+    secret: "SEU-CLIENT-SECRET",
+    webhook: "SEU-WEBHOOK",
+    prefix: ";",
+    owners: ["SEU-ID-AQUI"],
+    port: 3000,
+    authLink: "https://discord.com/api/oauth2/authorize?client_id=SEU-ID-DO-BOT&redirect_uri=SEU-LINK-AQUI&response_type=code&scope=identify%20guilds.join",
+}
